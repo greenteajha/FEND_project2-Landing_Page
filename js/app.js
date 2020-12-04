b37ec9cdd18bc856d8761ae7c4120bcc2b40f060
Jeremy Teo <jeremyteo88@hotmail.com>
2020-12-05 00:12:06 +0800

    Create the other sections and included more JavaScript.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 /* Hover effect over section 1's menu buttons */
 let section1menuitems = document.querySelectorAll(".section1-menu a");
+let section1imagetag = document.querySelector('#section1-embeded-image');
 
 for (let section1menuitem of section1menuitems){
     section1menuitem.addEventListener("mouseover", function(){
         section1menuitem.style.backgroundColor = "#730c02";
         section1menuitem.style.color = "#f2ae2e";
+        section1imagetag.setAttribute('src','images/section1-image-types-of-ingredients.png');
     });
 
     section1menuitem.addEventListener("mouseout", function(){
@@ -38,7 +40,6 @@ navLinks.setAttribute('id','nav-bar-section-links');
 
 /* Search for section names */
 const sectionNames = document.querySelectorAll('.sections');
-console.log(sectionNames[0].id);
 
 /* Create unordered list */
 const navLinksUL = document.createElement('ul');
@@ -48,6 +49,7 @@ for (let sectionName of sectionNames){
     let navLinksLI = document.createElement('li');
     let navLinksA = document.createElement('a');
     navLinksA.textContent = sectionName.id;
+    navLinksA.setAttribute('src','#section1-container');
     navLinksLI.appendChild(navLinksA);
     navLinksUL.appendChild(navLinksLI);
 }
@@ -58,3 +60,31 @@ navLinksFragment.appendChild(navLinks);
 const abc = document.querySelector('.nav-bar-container');
 abc.appendChild(navLinksFragment);
 
+
+/* Sticky Navigation Bar */
+document.onscroll = function() {myFunction()};
+
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
+
+  /* Scroll button back to the top */
+
+const topButton = document.getElementById('scroll-back-to-top-button');
+
+topButton.addEventListener("click", function(){
+    topFunction();
+});
+
+function topFunction() {
+    document.documentElement.scrollTo({
+        top: 0, behavior: "smooth"
+    })
+}
