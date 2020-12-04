0a8bc8794c3d6c5206817e5e3a3610e905aed506
Jeremy Teo <jeremyteo88@hotmail.com>
2020-12-04 09:54:25 +0800

    Added JavaScript functions

--- js/app.js
+++ js/app.js
@@ -1,14 +1,27 @@
-let a = document.querySelectorAll(".section1-menu a");
-console.log(a[0].innerText);
+/* Hover effect over section 1's menu buttons */
+let section1menuitems = document.querySelectorAll(".section1-menu a");
 
-for (let b of a){
-    b.addEventListener("mouseover", function(){
-        b.style.backgroundColor = "#730c02";
-        b.style.color = "#f2ae2e";
+for (let section1menuitem of section1menuitems){
+    section1menuitem.addEventListener("mouseover", function(){
+        section1menuitem.style.backgroundColor = "#730c02";
+        section1menuitem.style.color = "#f2ae2e";
     });
 
-    b.addEventListener("mouseout", function(){
-        b.style.backgroundColor = "#f2ae2e";
-        b.style.color = "#730c02";
+    section1menuitem.addEventListener("mouseout", function(){
+        section1menuitem.style.backgroundColor = "#f2ae2e";
+        section1menuitem.style.color = "#730c02";
     });
 }
+
+/* Document fragment for section 1's image*/
+
+const section1imagefragment = document.createDocumentFragment();
+
+const section1whatdowesell = document.createElement('img');
+section1whatdowesell.setAttribute('src','images/section1-image-what-do-we-sell.PNG');
+
+section1imagefragment.appendChild(section1whatdowesell);
+
+const section1imagenode = document.querySelector('#section1-information-image');
+
+section1imagenode.appendChild(section1imagefragment);
