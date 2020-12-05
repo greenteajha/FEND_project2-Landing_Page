b1abd77a0ebb89faac9a8c37c7816f4a3b28725b
Jeremy Teo <jeremyteo88@hotmail.com>
2020-12-06 00:13:13 +0800

    Change text in section 1

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const sectionOneText = document.querySelector('#section1-information-text');
 activeSectionOneOptionOne.addEventListener("mouseover",function(){
     activeSectionOneOptionOne.setAttribute('class','section1-menu-link-option-one active');
     sectionOneImage.setAttribute('src','images/section1-image-what-do-we-sell.PNG');
-    sectionOneText.textContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+    sectionOneText.textContent = "Bubble Tea. Chinese Deserts.";
     if (activeSectionOneOptionTwo.classList.contains('active')){
         activeSectionOneOptionTwo.setAttribute('class','section1-menu-link-option-two');
     }
@@ -18,7 +18,7 @@ activeSectionOneOptionOne.addEventListener("mouseover",function(){
 activeSectionOneOptionTwo.addEventListener("mouseover",function(){
     activeSectionOneOptionTwo.setAttribute('class','section1-menu-link-option-two active');
     sectionOneImage.setAttribute('src','images/section1-image-types-of-ingredients.png');
-    sectionOneText.textContent = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+    sectionOneText.textContent = "Fresh Ingredients. Locally Sourced. Organic";
     if (activeSectionOneOptionOne.classList.contains('active')){
         activeSectionOneOptionOne.setAttribute('class','section1-menu-link-option-one');
     }
